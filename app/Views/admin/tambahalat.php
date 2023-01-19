d2990828d4d3aab877b43a11b6ec24e4859d95d3
zakki31 <a.muzakki3103@gmail.com>
2023-01-19 12:08:41 +0700

    submit

--- app/Views/admin/tambahalat.php
+++ app/Views/admin/tambahalat.php
@@ -305,14 +305,9 @@
                 </div>
             </form>
         </div>
-        <div class="form-example-int mg-t-15">
-            <div class="row">
-                <div class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
-                </div>
-                <div class="col-lg-8 col-md-7 col-sm-7 col-xs-12">
-
-                </div>
-            </div>
+        <div class="col-lg-8 col-md-7 col-sm-7 col-xs-12">
+            <button  type="submit" class="btn btn-success notika-btn-success">Submit</button>
+        </div>
         </form>
     </div>
 </div>
