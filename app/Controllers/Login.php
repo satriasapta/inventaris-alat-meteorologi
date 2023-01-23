acb8f4a13e983114f18cbac312a8cf19404855fd
Satria Sapta <satriasapta48@gmail.com>
2023-01-23 22:48:18 +0700

    update view login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -8,6 +8,9 @@ class Login extends BaseController
 {
     public function index()
     {
+        if(session('id')){
+            return redirect()->to(site_url('/admin/dashboard'));
+        }
         return view('admin/loginAdmin');
     }
 }
