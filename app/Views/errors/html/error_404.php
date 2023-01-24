4d2b777efbed37841b7e49fa63ef3fc022612749
zakki31 <a.muzakki3103@gmail.com>
2023-01-24 17:41:56 +0700

    Update error_404.php

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -1,84 +1,132 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html class="no-js" lang="">
+
 <head>
     <meta charset="utf-8">
-    <title><?= lang('Errors.pageNotFound') ?></title>
-
-    <style>
-        div.logo {
-            height: 200px;
-            width: 155px;
-            display: inline-block;
-            opacity: 0.08;
-            position: absolute;
-            top: 2rem;
-            left: 50%;
-            margin-left: -73px;
-        }
-        body {
-            height: 100%;
-            background: #fafafa;
-            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            color: #777;
-            font-weight: 300;
-        }
-        h1 {
-            font-weight: lighter;
-            letter-spacing: normal;
-            font-size: 3rem;
-            margin-top: 0;
-            margin-bottom: 0;
-            color: #222;
-        }
-        .wrap {
-            max-width: 1024px;
-            margin: 5rem auto;
-            padding: 2rem;
-            background: #fff;
-            text-align: center;
-            border: 1px solid #efefef;
-            border-radius: 0.5rem;
-            position: relative;
-        }
-        pre {
-            white-space: normal;
-            margin-top: 1.5rem;
-        }
-        code {
-            background: #fafafa;
-            border: 1px solid #efefef;
-            padding: 0.5rem 1rem;
-            border-radius: 5px;
-            display: block;
-        }
-        p {
-            margin-top: 1.5rem;
-        }
-        .footer {
-            margin-top: 2rem;
-            border-top: 1px solid #efefef;
-            padding: 1em 2em 0 2em;
-            font-size: 85%;
-            color: #999;
-        }
-        a:active,
-        a:link,
-        a:visited {
-            color: #dd4814;
-        }
-    </style>
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>404 Page | ALMET</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+		============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="/templates2/img/bmkg.png">
+    <!-- Google Fonts
+		============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/bootstrap.min.css">
+    <!-- font awesome CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/font-awesome.min.css">
+    <!-- owl.carousel CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/owl.carousel.css">
+    <link rel="stylesheet" href="/templates2/css/owl.theme.css">
+    <link rel="stylesheet" href="/templates2/css/owl.transitions.css">
+    <!-- animate CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/animate.css">
+    <!-- normalize CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/normalize.css">
+    <!-- mCustomScrollbar CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/scrollbar/jquery.mCustomScrollbar.min.css">
+    <!-- wave CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/wave/waves.min.css">
+    <!-- Notika icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/notika-custom-icon.css">
+    <!-- main CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/main.css">
+    <!-- style CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/style.css">
+    <!-- responsive CSS
+		============================================ -->
+    <link rel="stylesheet" href="/templates2/css/responsive.css">
+    <!-- modernizr JS
+		============================================ -->
+    <script src="/templates2/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
-<body>
-    <div class="wrap">
-        <h1>404</h1>
 
-        <p>
-            <?php if (ENVIRONMENT !== 'production') : ?>
-                <?= nl2br(esc($message)) ?>
-            <?php else : ?>
-                <?= lang('Errors.sorryCannotFind') ?>
-            <?php endif ?>
-        </p>
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- 404 Page area Start-->
+    <div class="error-page-area">
+        <div class="error-page-wrap">
+            <i class="notika-icon notika-close"></i>
+            <h2>ERROR <span class="counter">404</span></h2>
+            <p>Sorry, but the page you are looking for has note been found. Try checking the URL for an error, then hit the refresh button on your browser or try found something else in our app.</p>
+            <a href="<?= base_url('admin/dashboard'); ?>" class="btn">Dashboard</a>
+            <a href="#" class="btn error-btn-mg">Report Problem</a>
+        </div>
     </div>
+    <!-- 404 Page area End-->
+    <!-- jquery
+		============================================ -->
+    <script src="/templates2/js/vendor/jquery-1.12.4.min.js"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="/templates2/js/bootstrap.min.js"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="/templates2/js/wow.min.js"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="/templates2/js/jquery-price-slider.js"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="/templates2/js/owl.carousel.min.js"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="/templates2/js/jquery.scrollUp.min.js"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="/templates2/js/meanmenu/jquery.meanmenu.js"></script>
+    <!-- counterup JS
+		============================================ -->
+    <script src="/templates2/js/counterup/jquery.counterup.min.js"></script>
+    <script src="/templates2/js/counterup/waypoints.min.js"></script>
+    <script src="/templates2/js/counterup/counterup-active.js"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="/templates2/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+    <!-- sparkline JS
+		============================================ -->
+    <script src="/templates2/js/sparkline/jquery.sparkline.min.js"></script>
+    <script src="/templates2/js/sparkline/sparkline-active.js"></script>
+    <!-- flot JS
+		============================================ -->
+    <script src="/templates2/js/flot/jquery.flot.js"></script>
+    <script src="/templates2/js/flot/jquery.flot.resize.js"></script>
+    <script src="/templates2/js/flot/flot-active.js"></script>
+    <!-- knob JS
+		============================================ -->
+    <script src="/templates2/js/knob/jquery.knob.js"></script>
+    <script src="/templates2/js/knob/jquery.appear.js"></script>
+    <script src="/templates2/js/knob/knob-active.js"></script>
+    <!--  wave JS
+		============================================ -->
+    <script src="/templates2/js/wave/waves.min.js"></script>
+    <script src="/templates2/js/wave/wave-active.js"></script>
+    <!--  Chat JS
+		============================================ -->
+    <script src="/templates2/js/chat/jquery.chat.js"></script>
+    <!--  todo JS
+		============================================ -->
+    <script src="/templates2/js/todo/jquery.todo.js"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="/templates2/js/plugins.js"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="/templates2/js/main.js"></script>
 </body>
+
 </html>
