d980f6482db81dfd0860bdf8f03a5f20437ebf4f
zakki31 <a.muzakki3103@gmail.com>
2023-01-31 12:08:35 +0700

    Update lbharian.php

--- app/Views/admin/lbharian.php
+++ app/Views/admin/lbharian.php
@@ -1,7 +1,78 @@
 <?= $this->extend('admin/templateadmin'); ?>
 
+
+
 <?= $this->section('content'); ?>
 <?php $index = 1 ?>
+
+<div class="data-table-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="data-table-list">
+                    <div class="basic-tb-hd">
+                        <?php if (session()->getFlashdata('success')) : ?>
+                            <div class="alert alert-success m-3" role="alert">
+                                <?= session()->getFlashdata('success'); ?>
+                            </div>
+                        <?php endif; ?>
+                        <h2>Daftar Alat</h2>
+                        <p>Daftar alat yang ada di BMKG Klas I Bandara Radin Inten II Bandar Lampung</p>
+                    </div>
+                    <div class="table-responsive">
+                        <table id="data-table-basic" class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Alat</th>
+                                    <th>Kelompok Cara Kerja Alat</th>
+                                    <th>Kondisi</th>
+                                    <th>Tahun Pembelian</th>
+                                    <th>Kalibrasi Terakhir</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($alat as $a) : ?>
+                                    <tr>
+                                        <td><?= $index++; ?></td>
+                                        <td><?= $a['nama_alat']; ?></td>
+                                        <td><?= $a['kategori_alat']; ?></td>
+                                        <td><?= $a['kondisi_alat']; ?></td>
+                                        <td><?= $a['tahun_pembelian']; ?></td>
+                                        <td><?= $a['kalibrasi']; ?></td>
+                                        <td>
+                                            <div class="material-design-btn">
+                                                <form action="<?= base_url('/admin/hapusalat/' . $a['id_alat']); ?>" method="POST" onsubmit="return confirm('Yakin ingin hapus data?')">
+                                                    <a href="<?= base_url('/admin/detailalat/' . $a['id_alat']); ?>" class="btn notika-btn-cyan waves-effect">Detail</a>
+                                                    <a href="<?= base_url('/admin/editalat/' . $a['id_alat']); ?>" class="btn notika-btn-teal waves-effect">Edit</a>
+                                                    <input type="hidden" name="_method" value="delete">
+                                                    <button class="btn notika-btn-red waves-effect">Hapus</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php endforeach ?>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Alat</th>
+                                    <th>Kelompok Cara Kerja Alat</th>
+                                    <th>Kondisi</th>
+                                    <th>Tahun Pembelian</th>
+                                    <th>Kalibrasi Terakhir</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="data-table-area">
     <div class="container">
         <div class="row">
@@ -16,22 +87,20 @@
                         <h2>Data Log Book Harian</h2>
                         <p>Log Book Harian BMKG Klas I Bandara Radin Inten II Bandar Lampung</p>
                     </div>
-                    <form action="" method="get" autocomplete="off">
-                        <?php $request = \Config\Services::request(); ?>
-                        <input type="text" name="keyword" value="<?= $request->getGet('keyword'); ?>" class="form-control">
-                        <button type="submit" class="btn btn-primary">search</button>
-                    </form>
+                    <div id="data-table-basic_filter" class="dataTables_filter">
+                        <label> "Search"
+                            <input type="search" class placeholder aria-controls="data-table-basic">
+                        </label>
+                    </div>
                     <div class="table-responsive">
                         <table id="data-table-basic" class="table table-striped">
                             <thead>
                                 <tr>
                                     <td>Nama Petugas </td>
-                                    <td>:</td>
                                     <td><?= $logbook['nama_petugas'] ?></td>
                                 </tr>
                                 <tr>
                                     <td>Tanggal Pengecekan </td>
-                                    <td>:</td>
                                     <td><?= $logbook['tanggal']; ?></td>
                                 </tr>
                                 <tr>
