3b3bf83453d67f3d82a00b7de2f99b03f7834bdf
zakki31 <a.muzakki3103@gmail.com>
2023-02-13 11:54:06 +0700

    profil at admin page

---
+++ app/Views/admin/profila.php
@@ -0,0 +1,131 @@
+<?= $this->extend('admin/templateadmin'); ?>
+<?= $this->section('content'); ?>
+
+<!-- Wizard area Start-->
+<div class="wizard-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="wizard-wrap-int">
+                    <div class="wizard-hd">
+                        <h2>BMKG Klas I Bandara Radin Inten II Bandar Lampung</h2>
+                        <p>UPT : Stasiun Meteorologi Kelas 1 Raden Inten II Lampung Selatan</p>
+                        <p>Tupoksi : Koordinator BMKG Lampung, Informasi Cuaca, Cuaca Bandara</p>
+                        <p>Alamat : Jl. Alamsyah Ratu Prawira Negara Km. 28 Branti, Natar, Lampung Selatan</p>
+                        <p>Phone/Fax : (0721) 7697093 / 0816 404 333 / (0721) 7697242</p>
+                    </div>
+                    <div id="rootwizard">
+                        <div class="navbar">
+                            <div class="navbar-inner">
+                                <div class="container-pro wizard-cts-st">
+                                    <ul>
+                                        <li><a href="#tab1" data-toggle="tab">Sejarah</a></li>
+                                        <li><a href="#tab2" data-toggle="tab">Visi & Misi</a></li>
+                                        <li><a href="#tab3" data-toggle="tab">Tugas & Fungsi</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-content">
+                            <div class="tab-pane wizard-ctn" id="tab1">
+                                <p>Sejarah pengamatan meteorologi dan geofisika di Indonesia dimulai pada tahun 1841 diawali dengan pengamatan yang dilakukan secara perorangan oleh Dr. Onnen, Kepala Rumah Sakit di Bogor. Tahun demi tahun kegiatannya berkembang sesuai dengan semakin diperlukannya data hasil pengamatan cuaca dan geofisika.</p>
+
+                                <p>Pada tahun 1866, kegiatan pengamatan perorangan tersebut oleh Pemerintah Hindia Belanda diresmikan menjadi instansi pemerintah dengan nama Magnetisch en Meteorologisch Observatorium atau Observatorium Magnetik dan Meteorologi dipimpin oleh Dr. Bergsma.</p>
+
+                                <p>Pada tahun 1879 dibangun jaringan penakar hujan sebanyak 74 stasiun pengamatan di Jawa. Pada tahun 1902 pengamatan medan magnet bumi dipindahkan dari Jakarta ke Bogor. Pengamatan gempa bumi dimulai pada tahun 1908 dengan pemasangan komponen horisontal seismograf Wiechert di Jakarta, sedangkan pemasangan komponen vertikal dilaksanakan pada tahun 1928.</p>
+
+                                <p>Pada tahun 1912 dilakukan reorganisasi pengamatan meteorologi dengan menambah jaringan sekunder. Sedangkan jasa meteorologi mulai digunakan untuk penerangan pada tahun 1930.</p>
+
+                                <p>Pada masa pendudukan Jepang antara tahun 1942 sampai dengan 1945, nama instansi meteorologi dan geofisika diganti menjadi Kisho Kauso Kusho.</p>
+
+                                <p>Setelah proklamasi kemerdekaan Indonesia pada tahun 1945, instansi tersebut dipecah menjadi dua: Di Yogyakarta dibentuk Biro Meteorologi yang berada di lingkungan Markas Tertinggi Tentara Rakyat Indonesia khusus untuk melayani kepentingan Angkatan Udara. Di Jakarta dibentuk Jawatan Meteorologi dan Geofisika, dibawah Kementerian Pekerjaan Umum dan Tenaga.</p>
+
+                                <p>Pada tanggal 21 Juli 1947 Jawatan Meteorologi dan Geofisika diambil alih oleh Pemerintah Belanda dan namanya diganti menjadi Meteorologisch en Geofisiche Dienst. Sementara itu, ada juga Jawatan Meteorologi dan Geofisika yang dipertahankan oleh Pemerintah Republik Indonesia, kedudukan instansi tersebut di Jl. Gondangdia, Jakarta.</p>
+
+                                <p>Pada tahun 1949, setelah penyerahan kedaulatan negara Republik Indonesia dari Belanda, Meteorologisch en Geofisiche Dienst diubah menjadi Jawatan Meteorologi dan Geofisika dibawah Departemen Perhubungan dan Pekerjaan Umum. Selanjutnya, pada tahun 1950 Indonesia secara resmi masuk sebagai anggota Organisasi Meteorologi Dunia (World Meteorological Organization atau WMO) dan Kepala Jawatan Meteorologi dan Geofisika menjadi Permanent Representative of Indonesia with WMO.</p>
+
+                                <p>Pada tahun 1955 Jawatan Meteorologi dan Geofisika diubah namanya menjadi Lembaga Meteorologi dan Geofisika di bawah Departemen Perhubungan, dan pada tahun 1960 namanya dikembalikan menjadi Jawatan Meteorologi dan Geofisika di bawah Departemen Perhubungan Udara.</p>
+
+                                <p>Pada tahun 1965, namanya diubah menjadi Direktorat Meteorologi dan Geofisika, kedudukannya tetap di bawah Departemen Perhubungan Udara.Pada tahun 1972, Direktorat Meteorologi dan Geofisika diganti namanya menjadi Pusat Meteorologi dan Geofisika, suatu instansi setingkat eselon II di bawah Departemen Perhubungan, dan pada tahun 1980 statusnya dinaikkan menjadi suatu instansi setingkat eselon I dengan nama Badan Meteorologi dan Geofisika, dengan kedudukan tetap berada di bawah Departemen Perhubungan.Pada tahun 2002, dengan keputusan Presiden RI Nomor 46 dan 48 tahun 2002, struktur organisasinya diubah menjadi Lembaga Pemerintah Non Departemen (LPND) dengan nama tetap Badan Meteorologi dan Geofisika.</p>
+
+                                <p>Terakhir, melalui Peraturan Presiden Nomor 61 Tahun 2008, Badan Meteorologi dan Geofisika berganti nama menjadi Badan Meteorologi, Klimatologi, dan Geofisika (BMKG) dengan status tetap sebagai Lembaga Pemerintah Non Departemen.Pada tanggal 1 Oktober 2009 Undang-Undang Republik Indonesia Nomor 31 Tahun 2009 tentang Meteorologi, Klimatologi dan Geofisika disahkan oleh Presiden Republik Indonesia, Susilo Bambang Yudhoyono. (unduh Penjelasan UU RI Nomor 31 Tahun 2009)</p>
+
+                                <p class="wizard-mg-ctn">Duis eu eros vitae risus sollicitudin blandit in non nisi. Phasellus rhoncus ullamcorper pretium. Etiam et viverra neque, aliquam imperdiet velit. Nam a scelerisque justo, id tristique diam. Aenean ut vestibulum velit, vel ornare augue. Nullam eu est malesuada, vehicula ex in, maximus massa. Sed sit amet massa venenatis, tristique orci sed, eleifend arcu.</p>
+                            </div>
+                            <div class="tab-pane wizard-ctn" id="tab2">
+                                <p>Dalam rangka mendukung dan mengemban tugas pokok dan fungsi serta memperhatikan kewenangan BMKG agar lebih efektif dan efisien, maka diperlukan aparatur yang profesional, bertanggung jawab dan berwibawa serta bebas dari Korupsi, Kolusi, dan Nepotisme (KKN), disamping itu harus dapat menjunjung tinggi kedisiplinan, kejujuran dan kebenaran guna ikut serta memberikan pelayanan informasi yang cepat, tepat dan akurat. Oleh karena itu kebijakan yang akan dilakukan BMKG Tahun 2010-2014 adalah mengacu pada Visi, Misi, dan Tujuan BMKG yang telah ditetapkan.</p>
+
+                                <h3>Visi</h3>
+                                <p>Mewujudkan BMKG yang handal, tanggap dan mampu dalam rangka mendukung keselamatan masyarakat serta keberhasilan pembangunan nasional, dan berperan aktif di tingkat Internasional.</p>
+                                <p><h5>Terminologi di dalam visi tersebut dapat dijelaskan sebagai berikut :</h5></p>
+                                <ul class="arrows_list list_style">
+                                    <p>
+                                        <li>Pelayanan informasi meteorologi, klimatologi, kualitas udara, dan geofisika yang handal ialah pelayanan BMKG terhadap penyajian data, informasi pelayanan jasa meteorologi, klimatologi, kualitas udara, dan geofisika yang akurat, tepat sasaran, tepat guna, cepat, lengkap, dan dapat dipertanggungjawabkan</li>
+                                    </p>
+                                    <p>
+                                        <li>Tanggap dan mampu dimaksudkan BMKG dapat menangkap dan merumuskan kebutuhan stakeholder akan data, informasi, dan jasa meteorologi, klimatologi, kualitas udara, dan geofisika serta mampu memberikan pelayanan sesuai dengan kebutuhan pengguna jasa;</li>
+                                    </p>
+                                </ul>
+                                <h3>Misi</h3>
+                                <p>Dalam rangka mewujudkan Visi BMKG, maka diperlukan visi yang jelas yaitu berupa langkah-langkah BMKG untuk mewujudkan Misi yang telah ditetapkan yaitu :</p>
+                                <ul class="arrows_list list_style">
+                                    <p>Mengamati dan memahami fenomena meteorologi, klimatologi, kualitas udara dan geofisika.</p>
+                                    <p>Menyediakan data, informasi dan jasa meteorologi, klimatologi, kualitas udara dan geofisika yang handal dan terpercaya.</p>
+                                    <p>Mengkoordinasikan dan memfasilitasi kegiatan di bidang meteorologi, klimatologi , kualitas udara dan geofisika.</p>
+                                    <p>Berpartisipasi aktif dalam kegiatan internasional di Bidang meteorologi, klimatologi , kualitas udara dan geofisika.</p>
+                                </ul>
+                                <br>
+                                <p><h5>Secara lebih rinci, maksud dari pernyataan misi di atas adalah sebagai berikut :</h5></p>
+
+                                <ul class="arrows_list list_style">
+                                    <p>Mengamati dan memahami fenomena meteorologi, klimatologi, kualitas udara, dan geofisika artinya BMKG melaksanakan operasional pengamatan dan pengumpulan data secara teratur, lengkap dan akurat guna dipakai untuk mengenali dan memahami karakteristik unsur-unsur meteorologi, klimatologi, kualitas udara, dan geofisika guna membuat prakiraan dan informasi yang akurat;</p>
+                                    <p>Menyediakan data, informasi dan jasa meteorologi, klimatologi, kualitas udara, dan geofisika kepada para pengguna sesuai dengan kebutuhan dan keinginan mereka dengan tingkat akurasi tinggi dan tepat waktu;</p>
+                                    <p>Mengkoordinasi dan Memfasilitasi kegiatan sesuai dengan kewenangan BMKG, maka BMKG wajib mengawasi pelaksanaan operasional, memberi pedoman teknis, serta berwenang untuk mengkalibrasi peralatan meteorologi, klimatologi, kualitas udara, dan geofisika sesuai dengan peraturan yang berlaku</p>
+                                    <p>Berpartisipasi aktif dalam kegiatan internasional artinya BMKG dalam melaksanakan kegiatan secara operasional selalu mengacu pada ketentuan internasional mengingat bahwa fenomena meteorologi, klimatologi, kualitas udara, dan geofisika tidak terbatas dan tidak terkait pada batas batas wilayah suatu negara manapun.</p>
+                                </ul>
+                            </div>
+                            <div class="tab-pane wizard-ctn" id="tab3">
+                                <p>BMKG mempunyai status sebuah Lembaga Pemerintah Non Departemen (LPND), dipimpin oleh seorang Kepala Badan.
+                                <p><h4>Tugas</h4></p>
+                                BMKG mempunyai tugas : melaksanakan tugas pemerintahan di bidang Meteorologi, Klimatologi, Kualitas Udara dan Geofisika sesuai dengan ketentuan perundang-undangan yang berlaku.
+                                <p><h4>Fungsi</h4></p>
+                                <p>melaksanakan tugas sebagaimana dimaksud di atas, Badan Meteorologi Klimatologi dan Geofisika menyelenggarakan fungsi : <ul class="arrows_list list_style"></p>
+                                    <li>Perumusan kebijakan nasional dan kebijakan umum di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Perumusan kebijakan teknis di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Koordinasi kebijakan, perencanaan dan program di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Pelaksanaan, pembinaan dan pengendalian observasi, dan pengolahan data dan informasi di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Pelayanan data dan informasi di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Penyampaian informasi kepada instansi dan pihak terkait serta masyarakat berkenaan dengan perubahan iklim;</li>
+                                    <li>Penyampaian informasi dan peringatan dini kepada instansi dan pihak terkait serta masyarakat berkenaan dengan bencana karena faktor meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Pelaksanaan kerja sama internasional di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Pelaksanaan penelitian, pengkajian, dan pengembangan di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Pelaksanaan, pembinaan, dan pengendalian instrumentasi, kalibrasi, dan jaringan komunikasi di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Koordinasi dan kerja sama instrumentasi, kalibrasi, dan jaringan komunikasi di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Pelaksanaan pendidikan dan pelatihan keahlian dan manajemen pemerintahan di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Pelaksanaan pendidikan profesional di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Pelaksanaan manajemen data di bidang meteorologi, klimatologi, dan geofisika;</li>
+                                    <li>Pembinaan dan koordinasi pelaksanaan tugas administrasi di lingkungan BMKG;</li>
+                                    <li>Pengelolaan barang milik/kekayaan negara yang menjadi tanggung jawab BMKG;</li>
+                                    <li>Pengawasan atas pelaksanaan tugas di lingkungan BMKG;</li>
+                                    <li>Penyampaian laporan, saran, dan pertimbangan di bidang meteorologi, klimatologi, dan geofisika.</li>
+                                </ul>
+                                <p class="wizard-mg-ctn">Dalam melaksanakan tugas dan fungsinya BMKG dikoordinasikan oleh Menteri yang bertanggung jawab di bidang perhubungan.</p>
+                            </div>
+
+                            <div class="wizard-action-pro">
+                                <ul class="wizard-nav-ac">
+                                    <li><a class="button-first a-prevent" href="#"><i class="notika-icon notika-more-button"></i></a></li>
+                                    <li><a class="button-previous a-prevent" href="#"><i class="notika-icon notika-back"></i></a></li>
+                                    <li><a class="button-next a-prevent" href="#"><i class="notika-icon notika-next-pro"></i></a></li>
+                                    <li><a class="button-last a-prevent" href="#"><i class="notika-icon notika-more-button"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
